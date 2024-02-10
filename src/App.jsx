2ad6fe64308dbb69357112358b0dd1c8ea8e4a26
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2024-02-10 19:42:10 +0100

    Initial setup: Configure project, install dependencies, set up SCSS variables, and add fonts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo repellat
+      vero eaque ratione autem odit nesciunt nisi dolorem molestiae corporis
+      illo necessitatibus quidem consequuntur exercitationem eos, unde
+      voluptates, aut nihil.
+    </div>
+  );
+}
+
+export default App;
